6bac6ef96deca6b9067be46f316440c812aec9e2
moratsam <vodopivec.rok@protonmail.com>
2021-10-02 19:17:00 +0200

    rbc0 refactoring

--- rbc0/rbc0_manager.go
+++ rbc0/rbc0_manager.go
@@ -16,27 +16,22 @@ type roundInfo struct{
 	//stage this node is in in regards to a rbc round.
 	//'1':INIT, '2':ECHO, '3':READY, '4':ACCEPTED
 	localStage uint32;
-	peersInStage map[uint32]int; //map [ STAGE -> number of all nodes in stage STAGE ]
+	peersPerStage map[uint32]int; //map [ STAGE -> number of all nodes in stage STAGE ]
+	payload string //content of the message of this round
+	//map [ SENDER_ID -> stage]
+	//store a map of which stage which peer is in
+	stageOfPeer	map[string]uint32
+
+	
 }
 
 type Manager struct{
 	logger	*zap.Logger
 	omniManager *omni.Manager
 
-	//map [PROTOCOL_ID -> message payload ]
-	msgPayloadMap map[string]string
-
-	//map [ PROTOCOL_ID -> map [ SENDER_ID -> stage] ]
-	//as per bracha's article, each rbc round is defined by unique ProtocolID
-	//for every round , store a map of which stage which sender is in
-	rbcMap		map[string]map[string]uint32
-
 	//map [ PROTOCOL_ID -> roundInfo ]
 	//for every round, keep info on it
 	roundInfoMap	map[string]*roundInfo
-
-	//set of accepted messages
-	acceptedRounds map[string]bool
 
 	peersNum int //number of peers in the network
 
@@ -53,10 +48,7 @@ func NewManager(logger *zap.Logger, peersNum int, omniManager *omni.Manager) *Ma
 	m := &Manager{
 		logger:				logger,
 		omniManager:		omniManager,
-		msgPayloadMap:		make(map[string]string),
-		rbcMap:				make(map[string](map[string]uint32)),
 		roundInfoMap:		make(map[string]*roundInfo),
-		acceptedRounds: 	make(map[string]bool),
 		peersNum:			peersNum,
 	}
 
@@ -84,22 +76,25 @@ func (m *Manager) msgReceiver(){
 				continue
 		}
 
-		if m.acceptedRounds[msg.ProtocolID] { //message from this round was already accepted
+		thisRoundInfo, exists := m.roundInfoMap[msg.ProtocolID]
+		if exists && thisRoundInfo.localStage == 4 { //this round was already accepted
 			continue
 		}
-		mapOfRound, exists := m.rbcMap[msg.ProtocolID]
 		if !exists{ //NEW MESSAGE ROUND (NEW PROTOCOL_ID)
 			m.logger.Debug("new message round")
-			m.msgPayloadMap[msg.ProtocolID] = msg.Payload
-			m.rbcMap[msg.ProtocolID] = make(map[string]uint32)
-			ri := &roundInfo{0, map[uint32]int{1:0, 2:0, 3:0}}
-			m.roundInfoMap[msg.ProtocolID] = ri
-			m.roundInfoMap[msg.ProtocolID].peersInStage[msg.Type]++
-		} else {
-			senderNodeStage, exists := mapOfRound[msg.SenderID]
+			var ri roundInfo
+			ri.localStage = 0
+			ri.peersPerStage = map[uint32]int{1:0, 2:0, 3:0}
+			ri.payload = msg.Payload
+			ri.stageOfPeer = make(map[string]uint32)
+
+			m.roundInfoMap[msg.ProtocolID] = &ri
+			m.roundInfoMap[msg.ProtocolID].peersPerStage[msg.Type]++
+		} else { //UPDATE ROUND
+			senderNodeStage, exists := thisRoundInfo.stageOfPeer[msg.SenderID]
 			if !exists || senderNodeStage < msg.Type{
-				m.rbcMap[msg.ProtocolID][msg.SenderID] = msg.Type
-				m.roundInfoMap[msg.ProtocolID].peersInStage[msg.Type]++
+				m.roundInfoMap[msg.ProtocolID].stageOfPeer[msg.SenderID] = msg.Type
+				m.roundInfoMap[msg.ProtocolID].peersPerStage[msg.Type]++
 			} else {
 				//sender resent a message from the same stage of a round. UNDEFINED BEHAVIOUR
 				m.logger.Warn("some weird sender stage fuckery")
@@ -111,6 +106,16 @@ func (m *Manager) msgReceiver(){
 	}
 }
 
+func (m *Manager) instantiateRoundInfo(protocolID, payload string){
+	var ri roundInfo
+	ri.localStage = 0
+	ri.peersPerStage = map[uint32]int{1:0, 2:0, 3:0}
+	ri.payload = payload
+	ri.stageOfPeer = make(map[string]uint32)
+	m.roundInfoMap[protocolID] = &ri
+}
+
+
 //follow the protocol description
 //after a message for a round was received, check if
 //enough inits/echos/readys have been received to move local node to next stage
@@ -118,9 +123,9 @@ func (m *Manager) msgReceiver(){
 //after local stage is ACCEPT, send signal to other parts of the node and cleanup maps
 func (m *Manager) checkRound(protocolID string){
 	localStage := m.roundInfoMap[protocolID].localStage
-	inits := m.roundInfoMap[protocolID].peersInStage[1]
-	echos := m.roundInfoMap[protocolID].peersInStage[2]
-	readys := m.roundInfoMap[protocolID].peersInStage[3]
+	inits := m.roundInfoMap[protocolID].peersPerStage[1]
+	echos := m.roundInfoMap[protocolID].peersPerStage[2]
+	readys := m.roundInfoMap[protocolID].peersPerStage[3]
 	if localStage == 4{ //received messages after already accepted, just ignore
 		return
 	}
@@ -138,14 +143,12 @@ func (m *Manager) checkRound(protocolID string){
 		m.roundInfoMap[protocolID].localStage = 4
 		m.logger.Info("round %s ACCEPTADO:",
 			zap.String("protocolID", protocolID),
-			zap.String("pld", m.msgPayloadMap[protocolID]),
+			zap.String("pld", m.roundInfoMap[protocolID].payload),
 		)
-		//cleanup maps
-		delete(m.msgPayloadMap, protocolID)
-		delete(m.rbcMap, protocolID)
-		delete(m.roundInfoMap, protocolID)
-		// set message as accepted to ignore further messages from this round
-		m.acceptedRounds[protocolID] = true
+		//cleanup round resources
+		m.roundInfoMap[protocolID].payload = ""
+		m.roundInfoMap[protocolID].peersPerStage = nil
+		m.roundInfoMap[protocolID].stageOfPeer = nil
 	} else if echos >= (n+t)/2 || readys >= t+1{ // enter READY stage
 		m.roundInfoMap[protocolID].localStage = 3
 		m.broadcast(protocolID, 3) //broadcast ready
@@ -160,7 +163,7 @@ func (m *Manager) broadcast(protocolID string, stage uint32){
 	msg := messages.MsgRbc0{
 		ProtocolID:		protocolID,
 		Type:				stage, //INIT, see proto/messages.proto
-		Payload:			m.msgPayloadMap[protocolID],
+		Payload:			m.roundInfoMap[protocolID].payload,
 	}
 	if err := m.omniManager.OmniPublisher(&msg); err != nil{
 		m.logger.Error("sending rbc0 msg in round FAILED", zap.String("protocolID", protocolID))
@@ -169,7 +172,7 @@ func (m *Manager) broadcast(protocolID string, stage uint32){
 
 func (m *Manager) InitBroadcast(nodeID, payload string) (bool, error){
 	protocolID := nodeID + "_" + strconv.Itoa(m.protocolCnt)
-	m.msgPayloadMap[protocolID] = payload //populate payload map for new message
+	m.instantiateRoundInfo(protocolID, payload)
 
 	m.broadcast(protocolID, 1)
 	m.logger.Debug("sending rbc0 INIT: DONE", zap.String("protocolID", protocolID))
