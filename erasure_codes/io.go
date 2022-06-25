981b58acd3cc364e24064b8e189ff4d44544130f
moratsam <vodopivec.rok@protonmail.com>
2022-06-25 15:09:37 +0200

    dela

--- erasure_codes/io.go
+++ erasure_codes/io.go
@@ -1,5 +1,6 @@
 package main
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
@@ -39,6 +40,7 @@ func readShards(shardpaths []string, c_row_indexes chan int, c_encoded_data chan
 	row_to_shardpath := make(map[int]int)
 	file_handles := make([]*os.File, n)
 
+	pizda := make([]byte, 0)
 	for i := range file_handles{
 		file_handles[i], err = os.Open(shardpaths[i]) //open file
 		if err != nil {
@@ -52,6 +54,17 @@ func readShards(shardpaths []string, c_row_indexes chan int, c_encoded_data chan
 			fmt.Println(err)
 		}
 		row_to_shardpath[int(row_index[0])] = i //store row index in map
+
+		paddingBuf := make([]byte, 8)
+		_, err = file_handles[i].Read(paddingBuf)
+		if err != nil {
+			fmt.Println(err)
+		}
+		padding := binary.LittleEndian.Uint64(paddingBuf)
+		if i == 0 {
+			c_row_indexes <- int(padding)
+		}
+		pizda = append(pizda, row_index[0])
 	}
 
 	//sort the indexes
@@ -70,30 +83,58 @@ func readShards(shardpaths []string, c_row_indexes chan int, c_encoded_data chan
 	var chunk data_chunk
 	chunk.data = make([]byte, const_CHUNK_SIZE)
 	files_closed := false
+	oo := make([]byte, 0)
 	for {
-		enc_byte := make([]byte, 1)
 		for _, k := range keys { //read one byte before moving to the next shard
 			file := file_handles[row_to_shardpath[k]]
-			_, err := file.Read(enc_byte)
+			readSize, err := file.Read(chunk.data[chunk.size:chunk.size+1])
 			if err != nil {
 				if err == io.EOF {
 					files_closed = true
 					//break can be used here because all shards are the same size, which means
 					//they all close at the same time (after same amount of bytes read).
+					//chunk.size += readSize
+					if readSize != 0 {
+						fmt.Println("pomoje tole ni kul")
+					}
+					//fmt.Println("zadnji", readSize)
 					break
 				} else {
 					fmt.Println(err)
 					return
 				}
 			}
-			chunk.data[chunk.size] = enc_byte[0]
-			chunk.size++
+			/*
+			for z:=0; z<readSize; z++ {
+				pizda = append(pizda, chunk.data[chunk.size+z])
+			}
+			*/
+			chunk.size += readSize
+			if len(oo) == 300 {
+				fmt.Println("rrrr:", oo)
+			}
 		}
 		
 		//it is save to consider chunk fulness only after every n bytes have been read
 		//because const_CHUNK_SIZE is a multiple of n.
-		if chunk.size == const_CHUNK_SIZE { 
-			c_encoded_data <- chunk
+		if chunk.size >= const_CHUNK_SIZE { 
+			for z:=0; z<chunk.size; z++ {
+				pizda = append(pizda, chunk.data[z])
+			}
+			/*
+			var copyChunk data_chunk
+			copyChunk.data = make([]byte, len(chunk.data))
+			for q:=0; q<chunk.size; q++ {
+				copyChunk.data[q] = chunk.data[q]
+			}
+			copyChunk.size = chunk.size
+			*/
+			copyChunk := data_chunk{
+				size: chunk.size,
+				data: make([]byte, chunk.size),
+			}
+			copy(copyChunk.data, chunk.data)
+			c_encoded_data <- copyChunk
 			chunk.size = 0
 		}
 		if files_closed{
@@ -102,11 +143,15 @@ func readShards(shardpaths []string, c_row_indexes chan int, c_encoded_data chan
 	}
 
 	if chunk.size > 0 {
+		fmt.Println("posl zadnga")
 		c_encoded_data <- chunk //send the last chunk, even if it's not full
 	}
+
+	//fmt.Println("s read", pizda)
 }
 
 func writeFile(path string, c chan byte, c_done chan struct{}) {
+	totalWrite := 0
 	file, err := os.Create(path)
 	if err != nil {
 		fmt.Println(err)
@@ -114,13 +159,18 @@ func writeFile(path string, c chan byte, c_done chan struct{}) {
 	defer file.Close()
 	defer close(c_done)
 
+	pizda := make([]byte, 0)
 	buf := make([]byte, const_CHUNK_SIZE)
+	//buf := make([]byte, 1)
 	ix := 0
 	for b := range c { //receive byte, write to file
+		pizda = append(pizda, b)
 		buf[ix] = b
 		ix++
-		if ix == len(buf)-1 {
+
+		if ix >= len(buf) {
 			ix = 0
+			totalWrite += len(buf)
 			_, err := file.Write(buf)
 			if err != nil {
 				fmt.Println(err)
@@ -128,28 +178,67 @@ func writeFile(path string, c chan byte, c_done chan struct{}) {
 		}
 	}
 
+	//fmt.Println("total pre inc:", totalWrite, ix)
 	if ix > 0 {
+		totalWrite += ix
 		_, err := file.Write(buf[:ix])
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
 
+	//fmt.Println("total write", totalWrite)
+	//fmt.Println("output", pizda)
 }
 
 //read bytes from file, send them to channel c
-func readFile(path string, c chan data_chunk) {
+func readFile(path string, c chan data_chunk, n byte) {
 	file, err := os.Open(path)
 	defer file.Close()
 	if err != nil {
 		errors.New("napakica v branju fajla frent")
 	}
 
+	// Get file size
+	stat, err := file.Stat()
+	if err != nil {
+		errors.New("napakica v statu frent")
+	}
+	fmt.Println("fsize", stat.Size())
+	fSize := stat.Size()
+
+	// Some padding is necessary.
+	padding := int(int64(n) - (fSize % int64(n)))
+	fmt.Println("padding neccessary", padding)
+	if padding > 0 {
+		var chunk data_chunk
+		chunk.data = make([]byte, const_CHUNK_SIZE)
+
+		chunk.size, err = file.Read(chunk.data[padding:])
+		chunk.size += padding
+		if err != nil {
+			if err == io.EOF{
+				if chunk.size > 0{
+					c <- chunk
+				}
+				close(c)
+				return
+			} else{
+				fmt.Println(err)
+			}
+		}
+		c <- chunk
+	}
+
+	pizda := make([]byte, 0)
 	for {
 		var chunk data_chunk
 		chunk.data = make([]byte, const_CHUNK_SIZE)
 
 		chunk.size, err = file.Read(chunk.data)
+		for x := 0; x < chunk.size; x++ {
+			pizda = append(pizda, chunk.data[x])
+		}
 		if err != nil {
 			if err == io.EOF{
 				if chunk.size > 0{
@@ -163,5 +252,6 @@ func readFile(path string, c chan data_chunk) {
 		}
 		c <- chunk
 	}
+	//fmt.Println("input", pizda)
 }
 
--- erasure_codes/manager.go
+++ erasure_codes/manager.go
@@ -1,7 +1,9 @@
 package main
 import (
+	"encoding/binary"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -18,7 +20,7 @@ type Manager struct {
 }
 
 func NewManager(k, n byte) *Manager {
-	const_CHUNK_SIZE = 1000*int(n)
+	const_CHUNK_SIZE = 121*int(n)
 	if int(k) + int(2*n) > 255 {
 		panic("the sum of k and n must not exceed 255")
 	}
@@ -36,7 +38,7 @@ func NewManager(k, n byte) *Manager {
 //it spawns a subroutine for each for in the cauchy matrix
 //each subroutine will read the current chunk, break it into n-words, encode the n-words by
 //making a dot product with its cauchy row, then send the encoded byte to its writer routine.
-func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte){
+func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte, fSize uint64){
 	k, n := int(m.k), int(m.n)
 	//each time the routine running encode() receives a new data chunk, it will use the
 	//c_data_available to tell each of the row routines that a new chunk is available.
@@ -51,6 +53,12 @@ func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte){
 		go func(c_data_available chan struct{}, c_writer chan byte, data *data_chunk, wg *sync.WaitGroup, i int) { 
 			c_writer <- byte(i) //send index of cauchy matrix row to be stored in the shard
 			cauchy_row := m.mat[i]
+
+			bs := make([]byte, 8)
+			binary.LittleEndian.PutUint64(bs, fSize)
+			for _,b := range bs {
+				c_writer <- b
+			}
 
 			for {
 				_, ok := <- c_data_available //new data is available
@@ -71,6 +79,7 @@ func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte){
 		}(c_data_available[i], c_writers[i], &chunk, wg, i)
 	}
 
+	totalRead := 0
 	ok := true
 	for {
 		chunk, ok = <- c_reader //receive chunk
@@ -78,7 +87,7 @@ func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte){
 			for _,c := range c_data_available{
 				close(c)
 			}
-			return
+			break
 		}
 		//if the filesize of the file being encoded is not a multiple of n,
 		//the last word of the last chunk would not be a n-word,
@@ -92,7 +101,9 @@ func (m *Manager) encode(c_reader chan data_chunk, c_writers []chan byte){
 			c <- struct{}{}
 		}
 		wg.Wait() //wait for them to finish
+		totalRead += chunk.size
 	}
+	fmt.Println("total read", totalRead)
 }
 
 //readShards will read the shards, and send back their indexes, so that the appropriate
@@ -105,12 +116,15 @@ func (m *Manager) Decode(shard_paths []string, outpath string){
 
 	c_row_indexes := make(chan int) //c via which readShards() sends shard (row) indexes
 	c_encoded_data := make(chan data_chunk) //c via which readShards() sends shard data
-	c_writer := make(chan byte, 2*int(m.n)) //c via which decoded data is sent to writeFile()
+	c_writer := make(chan byte, 1*int(m.n)) //c via which decoded data is sent to writeFile()
 	c_writer_done := make(chan struct{}) //c used by writeFile() to signal when it is done
 
 	go readShards(shard_paths, c_row_indexes, c_encoded_data)
 	go writeFile(outpath, c_writer, c_writer_done) 
 
+	padding := <- c_row_indexes
+	paddingBuf := make([]byte, 0, padding)
+	fmt.Println("read padding", padding)
 	i, row_indexes := 0, make([]int, m.n) //indexes of cauchy rows that encoded the files
 	for row_index := range c_row_indexes {
 		row_indexes[i] = row_index
@@ -119,14 +133,31 @@ func (m *Manager) Decode(shard_paths []string, outpath string){
 
 	inv := create_inverse(m.mat, row_indexes)
 
+	oo := make([]byte, 0)
+	doit := true
 	for chunky := range c_encoded_data{
+		for z := 0; z < chunky.size; z++{
+			//fmt.Println("ooo")
+			oo = append(oo, chunky.data[z])
+		}
+		if len(oo) >= 300 && doit {
+			//fmt.Println("z:", oo)
+			doit = false
+		}
 		for ix:=0; ix<chunky.size; ix+=int(m.n) {
-			data_word := decode_word(inv, chunky.data[ix:ix+int(m.n)]) //decoded
+			enc_word := chunky.data[ix:ix+int(m.n)]
+			data_word := decode_word(inv, enc_word) //decoded
+			//fmt.Println("enc word", enc_word, "dec word", data_word)
 			for _, b := range data_word { //send to writer
+				if len(paddingBuf) != cap(paddingBuf) {
+					paddingBuf = append(paddingBuf, b)
+					continue
+				}
 				c_writer <- b
 			}
 		}
 	}
+	//fmt.Println("xxxxx", oo)
 	close(c_writer)
 	for _ = range c_writer_done {} //wait for chan to close
 }
@@ -139,14 +170,21 @@ func (m *Manager) Encode(inpath string) ([]string, error) {
 	c_writers_done := make([]chan struct{}, m.n+m.k) //c for each fileWrite() to signal when it is done
 
 	for i := range c_writers {
-		c_writers[i] = make(chan byte, 3*int(m.n))
+		c_writers[i] = make(chan byte, int(m.n))
 		outpaths[i] = inpath + "_" + strconv.Itoa(i) + ".enc"
 		c_writers_done[i] = make(chan struct{})
 		go writeFile(outpaths[i], c_writers[i], c_writers_done[i]) //spawn n+k routines which will write shards
 	}
 
-	go readFile(inpath, c_reader) //make routine which will read file
-	go m.encode(c_reader, c_writers) //read data, encode it, send it to writers
+	fi, err := os.Stat(inpath);
+	if err != nil {
+		fmt.Println("Can't get file size")
+	}
+	fSize := int64(fi.Size())
+	padding := uint64(int64(m.n) - (fSize % int64(m.n)))
+
+	go readFile(inpath, c_reader, m.n) //make routine which will read file
+	go m.encode(c_reader, c_writers, padding) //read data, encode it, send it to writers
 
 	//wait for every writer to be done
 	for _, c := range c_writers_done{
@@ -158,11 +196,14 @@ func (m *Manager) Encode(inpath string) ([]string, error) {
 
 
 func main(){
-	m := NewManager(5, 8)
+	m := NewManager(3, 7)
 	outpaths, _ := m.Encode("fajl")
 	
+
 	//create random subset of out files to use for decoding
-	rand.Seed(time.Now().UnixNano())
+	//rand.Seed(time.Now().UnixNano())
+	_ = time.Now()
+	rand.Seed(3)
 	subset := make([]string, m.n)
 	for i, path_ix := range rand.Perm(int(m.k+m.n)){
 		if i == int(m.n) {
@@ -173,5 +214,39 @@ func main(){
 	//fmt.Println(subset)
 
 	m.Decode(subset, "dekodiran")
+}
+
+
+func main1(){
+	const_CHUNK_SIZE = 121
+	inpath := "fajl.jpg"
+	outpath := "out.jpg"
+	c_reader := make(chan data_chunk) //c via which readFile() will send data
+	c_writer := make(chan byte)
+	c_writer_done := make(chan struct{})
+	
+	fi, err := os.Stat(inpath);
+	if err != nil {
+		fmt.Println("Can't get file size")
+	}
+	fSize := fi.Size()
+	fmt.Println("fsize", fSize)
+
+	go readFile(inpath, c_reader, 1)
+	go writeFile(outpath, c_writer, c_writer_done)
+
+	total := 0
+	for chunk := range c_reader{
+		for i:=0; i<chunk.size; i++ {
+			c_writer <- chunk.data[i]
+		}
+		total += chunk.size
+	}
+	close(c_writer)
+
+	 <- c_writer_done
+	 fmt.Println("total: ", total)
+
+
 }
 
